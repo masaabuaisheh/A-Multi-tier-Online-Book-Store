fd8f14df28f910b47338fa24d41fc8f0d94f9b64
masaabuaisheh <masaabuaisheh11@gmail.com>
2024-11-13 20:08:58 +0200

    part2 new commit

---
+++ config/dbconnection3.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: process.env.DATABASE_HOST2,
+  user: process.env.DATABASE_USER2,
+  password: process.env.DATABASE_PASSWORD2,
+  database: process.env.DATABASE2,
+});
+
+
+connection.connect((error) => {
+  if (error) {
+    console.error("Connection Error: ", error);
+    return;
+  }
+  console.log(process.env.DATABASE2);
+  console.log(process.env.DATABASE2 + " MYSQL Connected3...");
+});
+
+module.exports = connection;
